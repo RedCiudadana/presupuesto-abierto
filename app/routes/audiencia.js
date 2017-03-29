03804e6241f0025f472034d48c5e03deefbec452
Hector Hurtarte <hectorh30@gmail.com>
2017-03-28 18:26:19 -0600

    Agregar ruta para audiencia

---
+++ app/routes/audiencia.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model(params) {
+    return this.store.peekRecord('audiencia', params.id);
+  },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+  }
+});
